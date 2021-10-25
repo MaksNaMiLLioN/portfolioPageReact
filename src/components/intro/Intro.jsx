cc80596cab773d7cc5a8eddb26b0a670ffeeb190
Nuraly Tyulkubayev <tsnurali@mail.ru>
2021-10-25 21:20:32 +0600

    intro created

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './intro.css'
+//import Man from '../../img/phone.png'
+
+const Intro = () => {
+    return (
+        <div className="i">
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h2 className="i-into">My name is </h2>
+                    <h1 className="i-name">John Doe </h1>
+                    <div className="i-title">
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">UI/UX Designer</div>
+                            <div className="i-title-item">Photographer</div>
+                            <div className="i-title-item">Writer</div>
+                            <div className="i-title-item">Content Creator</div>
+                        </div>
+                    </div>
+                    <div className="i-desc">
+                        I design a develop services for customers of all sizes, specializing 
+                        in creating stylish, moder websites, web services and online stores.
+                    </div>
+                </div>
+            </div>
+            <div className="i-right">
+                <img src="" alt="" className="i-img"/>
+            </div>
+        </div>
+            
+    )
+}
+
+export default Intro
